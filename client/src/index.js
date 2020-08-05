b213556383f7902a2fd363a15ffd6da74a14fad8
Aaron Flowers <flowersa29@gmail.com>
2020-08-05 18:50:39 -0500

    8/5

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,16 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+    <Router>
+        <App/>
+    </Router>, rootElement
+);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
